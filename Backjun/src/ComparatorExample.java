300cab63255aa0382481e9bb538c7ec93e962b02
Mintaek_CHO <mintaek5555@gmail.com>
2022-07-18 21:48:46 +0900

    스트림 연습

--- Backjun/src/ComparatorExample.java
+++ Backjun/src/ComparatorExample.java
@@ -4,11 +4,19 @@ import  java.util.stream.Stream;
 
 public class ComparatorExample {
     public static void main(String[] args) {
-        Stream<String[]> stringArraysStream = Stream.of(
-                new String[]{"hello", "world", "java"},
-                new String[]{"code", "states"});
+        List<Integer> age = Arrays.asList(5, 15, 25, 45);
+        age.stream()
+                .filter(a -> a > 15)
+                .forEach(a -> System.out.println(a));
 
-        stringArraysStream.flatMap(Arrays::stream).forEach(System.out::println);
+        List<String>  name = Arrays.asList("larva", "duck", "cat");
+
+        name.stream()
+
+                .peek(s -> System.out.println(s))
+                .map(s -> s.toUpperCase(Locale.ROOT))
+                .forEach(s -> System.out.println(s));
+
     }
 
 }
